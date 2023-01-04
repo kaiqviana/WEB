d15bebd85721e69ef103f071fa203bd6019bc34f
Kaique Viana <kaiquevdesign9@gmail.com>
2023-01-04 16:05:19 -0300

    Novo

--- JavaScript/ObjetoMath.js
+++ JavaScript/ObjetoMath.js
@@ -4,7 +4,8 @@ console.log(Math.ceil(2.5));
 console.log(Math.floor(2.5));
 console.log(Math.abs(-2.5));
 console.log(Math.max(1,4,6));
-console.log(Math.min())
-console.log()
-console.log()
-console.log()
+console.log(Math.min(1,4,6));
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.sqrt(25));
+console.log(Math.pow(27,1/3)); 
