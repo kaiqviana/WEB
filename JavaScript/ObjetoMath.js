d6d08df8df7ca1ee65438a3f3214575018349baf
Kaique Viana <kaiquevdesign9@gmail.com>
2023-01-04 14:02:04 -0300

    Novo

---
+++ JavaScript/ObjetoMath.js
@@ -0,0 +1,10 @@
+console.log(Math.pow(2,5));
+console.log(Math.round(2.5));
+console.log(Math.ceil(2.5));
+console.log(Math.floor(2.5));
+console.log(Math.abs(-2.5));
+console.log(Math.max(1,4,6));
+console.log(Math.min())
+console.log()
+console.log()
+console.log()
